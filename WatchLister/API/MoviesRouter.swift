adb091303720a5e51404c0e45873225545104d4b
Francis Breidenbach <fbreidenbach@gmail.com>
2017-10-14 20:58:54 -0700

    Implement getMovies in MoviesRouter.

--- WatchLister/API/MoviesRouter.swift
+++ WatchLister/API/MoviesRouter.swift
@@ -10,8 +10,57 @@ import Foundation
 import Alamofire
 
 
-enum MoviesRouter: RouterType {
+enum MoviesRouter: RouterType, URLRequestConvertible {
   
-  case get
+  // MARK: - Cases
   
+  case getMovies
+  
+  
+  // MARK: - Properties
+  
+  var method: HTTPMethod {
+    switch self {
+    case .getMovies:
+      return .get
+    }
+  }
+  
+  
+  var path: String {
+    switch self {
+    case .getMovies:
+      return "discover/movie/"
+    }
+  }
+  
+  
+  var parameters: Parameters {
+    var params = Parameters()
+    
+    params["api_key"] = privateKey
+    
+    switch self {
+    case .getMovies:
+      params["sort_by"] = "popularity.desc"
+      params["include_adult"] = false
+    }
+    
+    return params
+  }
+  
+  
+  func asURLRequest() throws -> URLRequest {
+    let requestURL = baseURL.appendingPathComponent(path)
+    
+    var baseRequest = URLRequest(url: requestURL)
+    baseRequest.httpMethod = method.rawValue
+    
+    let encoding = URLEncoding.queryString
+    
+    return try encoding.encode(baseRequest, with: parameters)
+  }
 }
+
+
+
