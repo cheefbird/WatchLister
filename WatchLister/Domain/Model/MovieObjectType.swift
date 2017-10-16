2b9147cffa0ab895040fb33f1545e421236e17b7
Francis Breidenbach <fbreidenbach@gmail.com>
2017-10-15 18:41:31 -0700

    Create Domain directory and MovieObjectType

---
+++ WatchLister/Domain/Model/MovieObjectType.swift
@@ -0,0 +1,21 @@
+//
+//  MovieObjectType.swift
+//  WatchLister
+//
+//  Created by Francis Breidenbach on 10/15/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+
+
+protocol MovieObjectType {
+    var title: Int { get }
+    var posterPath: String { get }
+    var summary: String { get }
+    var releaseDate: String { get }
+    var id: Int { get }
+    var backdropPath: String { get }
+    var score: Float { get }
+    var isFavorite: Bool { get }
+}
