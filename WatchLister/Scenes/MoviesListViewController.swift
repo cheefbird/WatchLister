f7c6e6c680aaf5eafbd4bd510008348a94b21532
Francis Breidenbach <fbreidenbach@gmail.com>
2017-10-17 18:31:40 -0700

    Cleanup and setup for MoviesListVC

---
+++ WatchLister/Scenes/MoviesListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MoviesListViewController.swift
+//  WatchLister
+//
+//  Created by Francis Breidenbach on 10/14/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+  
+  // MARK: - Outlets
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+  // MARK: - Properties
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
+
+extension MoviesListViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+  
+}
+
